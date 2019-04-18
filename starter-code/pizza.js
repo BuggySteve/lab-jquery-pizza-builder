3e16347ecace38e16a33ccc850629ba829d2f632
Steven Lindelauff <slindelauff@gmail.com>
2019-04-18 14:45:48 +0200

    finished iteration 5

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,37 +1,93 @@
+var pepperoniBtn = $(".btn-pepperonni");
+var mushroomBtn = $(".btn-mushrooms");
+var greenPepperBtn = $(".btn-green-peppers");
+var sauceBtn = $(".btn-sauce");
+var crustBtn = $(".btn-crust");
+
+var totalPrice = 13;
+var totalPriceEl = $("#total-price");
 
 function togglePepperoni () {
   $(".pep").toggle("pep");
-  $(".btn-pepperonni").toggleClass("active");
+  pepperoniBtn.toggleClass("active");
   $("li:contains('$1 pepperoni')").toggle("li:contains('$1 pepperoni')");
 }
 
 function toggleMushrooms () {
   $(".mushroom").toggle("mushroom");
-  $(".btn-mushrooms").toggleClass("active");
+  mushroomBtn.toggleClass("active");
   $("li:contains('$1 mushrooms')").toggle("li:contains('$1 mushrooms')");
 }
 
 function toggleGreenPeppers () {
   $(".green-pepper").toggle("green-pepper");
-  $(".btn-green-peppers").toggleClass("active");
+  greenPepperBtn.toggleClass("active");
   $("li:contains('$1 green peppers')").toggle("li:contains('$1 green peppers')");
 }
 
 function toggleWhiteSauce () {
   $(".sauce").toggleClass("sauce-white");
-  $(".btn-sauce").toggleClass("active");
+  sauceBtn.toggleClass("active");
   $("li:contains('$3 white sauce')").toggle("li:contains('$3 white sauce')");
 }
 
 function toggleGlutenFree () {
   $(".crust").toggleClass("crust-gluten-free");
-  $(".btn-crust").toggleClass("active");
+  crustBtn.toggleClass("active");
   $("li:contains('$5 gluten-free crust')").toggle("li:contains('$5 gluten-free crust')");
 }
 
- $(".btn-pepperonni").click(togglePepperoni);
- $(".btn-mushrooms").click(toggleMushrooms);
- $(".btn-green-peppers").click(toggleGreenPeppers);
- $(".btn-sauce").click(toggleWhiteSauce);
- $(".btn-crust").click(toggleGlutenFree);
+pepperoniBtn.click(function() {
+  togglePepperoni();
+  if(pepperoniBtn.hasClass("active")) {
+    totalPrice += 1;
+  }
+  else {
+    totalPrice -= 1;
+  }
+  totalPriceEl.html(totalPrice);
+})
 
+mushroomBtn.click(function () {
+  toggleMushrooms();
+  if(mushroomBtn.hasClass("active")) {
+    totalPrice += 1;
+  }
+  else {
+    totalPrice -= 1;
+  }
+  totalPriceEl.html(totalPrice);
+});
+
+greenPepperBtn.click(function () {
+  toggleGreenPeppers();
+  if(greenPepperBtn.hasClass("active")) {
+    totalPrice += 1;
+  }
+  else {
+    totalPrice -= 1;
+  }
+  totalPriceEl.html(totalPrice);
+});
+
+sauceBtn.click(function () {
+  toggleWhiteSauce();
+  if(sauceBtn.hasClass("active")) {
+    totalPrice += 3;
+  }
+  else {
+    totalPrice -= 3;
+  }
+  totalPriceEl.html(totalPrice);
+});
+
+crustBtn.click(function () {
+  toggleGlutenFree();
+  if(crustBtn.hasClass("active")) {
+    totalPrice += 5;
+  }
+  else {
+    totalPrice -= 5;
+  }
+  totalPriceEl.html(totalPrice);
+})
